f699038a69b697b36bd905b3f74388948c293486
YeaWon Kim <yeawon6566@gmail.com>
2024-02-22 22:06:59 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        if (num1>=0 && num1<=10000 && num2>=0 && num2<=10000){
-            if (num1 == num2){
-                answer = 1;
-            }
-            else if (num1 != num2){
-                answer = -1;
-            }
-        }
+        int answer = (num1 == num2) ? 1 : -1;
         return answer;
     }
 }
