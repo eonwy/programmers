01d8798731c04dc955eaa3652bcee74628c5c8e7
YeaWon Kim <yeawon6566@gmail.com>
2024-03-01 23:12:51 +0900

    [level 0] Title: 가위 바위 보, Time: 3.52 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,21 +1,21 @@
 class Solution {
     public String solution(String rsp) {
         
-        StringBuilder answer = new StringBuilder();
+        String answer = "";
         
         for(int i=0; i<rsp.length(); i++){
             char current = rsp.charAt(i);
             if(current == '2'){
-                answer.append('0');
+                answer += '0';
             }
             else if(current == '0'){
-                answer.append('5');
+                answer += '5';
             }
             else if(current == '5'){
-                answer.append('2');
+                answer += '2';
             }
         }
         
-        return answer.toString();
+        return answer;
     }
 }
