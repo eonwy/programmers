7e08d7f964598490e58b8da2f0e10a8d9220af8d
YeaWon Kim <yeawon6566@gmail.com>
2024-03-20 16:55:03 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        
+        int max = array[0];
+        int index = 0;
+        
+        for(int i=0; i<array.length; i++){
+            if(max < array[i]){
+                max = array[i];
+                index = i;
+            }
+        }
+        
+        return new int[]{max, index};
+    }
+}
