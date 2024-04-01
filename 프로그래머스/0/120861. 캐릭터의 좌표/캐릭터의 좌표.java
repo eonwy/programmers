e79d32f01639599fe10192edc8024401256e8707
YeaWon Kim <yeawon6566@gmail.com>
2024-04-01 19:08:36 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        
+        int x = 0; 
+        int y = 0;
+        int bx = board[0];
+        int by = board[1];
+        
+        for(int i=0; i<keyinput.length; i++){
+            if(keyinput[i].equals("left")){
+                x -= 1;
+            }
+            else if(keyinput[i].equals("right")){
+                x += 1;
+            }
+            else if(keyinput[i].equals("down")){
+                y -= 1;
+            }
+            else if(keyinput[i].equals("up")){
+                y += 1;
+            }
+            if(Math.abs(x) > bx / 2){
+                if(x < 0){
+                    x = -(bx / 2);
+                }
+                else{
+                    x = bx / 2;
+                }
+            }
+            if(Math.abs(y) > by / 2){
+                if(y < 0){
+                    y = -(by / 2);
+                }
+                else{
+                    y = by / 2;
+                }
+            }
+        }
+        
+        
+        // if(Math.abs(x) > bx / 2){
+        //     if(x < 0){
+        //         x = -(bx / 2);
+        //     }
+        //     else{
+        //         x = bx / 2;
+        //     }
+        // }
+        // if(Math.abs(y) > by / 2){
+        //     if(y < 0){
+        //         y = -(by / 2);
+        //     }
+        //     else{
+        //         y = by / 2;
+        //     }
+        // }
+        
+        int[] answer = new int[]{x, y};
+        return answer;
+    }
+}
