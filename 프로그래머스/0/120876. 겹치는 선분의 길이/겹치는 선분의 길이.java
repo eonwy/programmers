4cd8905c8ee906e97159c462d59f911b3fb506f7
YeaWon Kim <yeawon6566@gmail.com>
2024-04-11 19:17:54 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int arr[] = new int[200];
+        
+        for(int i=0; i<lines.length; i++){
+            for(int j=lines[i][0]+100; j<lines[i][1]+100; j++){
+                arr[j]++;
+            }
+        }
+        
+        for(int z=0; z<arr.length; z++){
+            if(arr[z] > 1){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
