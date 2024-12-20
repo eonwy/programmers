75a9e7e3a3026c1fb142f3dafd0a07e533e8217e
YeaWon Kim <yeawon6566@gmail.com>
2024-12-21 00:24:28 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.50 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
         Arrays.sort(num_list);
-        answer = Arrays.copyOfRange(num_list, 0, 5);
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
