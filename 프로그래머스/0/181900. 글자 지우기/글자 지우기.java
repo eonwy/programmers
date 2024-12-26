042da5145401d8ea37b3af8f33c786a97acc412d
YeaWon Kim <yeawon6566@gmail.com>
2024-12-26 23:28:45 +0900

    [level 0] Title: 글자 지우기, Time: 0.56 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder answer = new StringBuilder(my_string);
+        
+        Arrays.sort(indices);
+        
+        for(int i=indices.length - 1; i>=0; i--) {
+            answer.deleteCharAt(indices[i]);
+        }
+        
+        return answer.toString();
+    }
+}
