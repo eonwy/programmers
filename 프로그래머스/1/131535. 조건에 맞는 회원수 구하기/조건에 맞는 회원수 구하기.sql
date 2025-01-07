183ef57beee0954eb63684e851bc0dad3ac27530
YeaWon Kim <yeawon6566@gmail.com>
2025-01-07 20:21:29 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT 
+    COUNT(*) 
+    AS USERS 
+FROM 
+    USER_INFO 
+WHERE 
+    USER_INFO.JOINED >= '2021-01-01'
+    AND USER_INFO.JOINED <= '2021-12-31'
+    AND USER_INFO.AGE <= 29 
+    AND USER_INFO.AGE >= 20;
