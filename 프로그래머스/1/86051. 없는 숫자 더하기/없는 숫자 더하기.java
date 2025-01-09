364f08ebb351878e37873e6af5ba08654eecc97d
YeaWon Kim <yeawon6566@gmail.com>
2025-01-09 13:06:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.56 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        for(int i=0; i<numbers.length; i++) {
+            answer += numbers[i];
+        }
+        
+        return 45-answer;
+    }
+}
