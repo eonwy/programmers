d659ccba8981ef6636aee465069e7d651f5486a1
YeaWon Kim <yeawon6566@gmail.com>
2025-01-09 13:35:51 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        if (s.length() % 2 == 0) {
+            answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+        } else {
+            answer = String.valueOf(s.charAt(s.length() / 2));
+        }
+        
+        return answer;
+    }
+}
