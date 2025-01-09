a269eeb754d1e018b83f4a9462adf9342dc5771c
YeaWon Kim <yeawon6566@gmail.com>
2025-01-09 12:36:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.45 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int num = 0;
+        
+        for(int i=0; i<seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                num = i;
+            }
+        }
+        
+        return "김서방은 "+num+"에 있다";
+    }
+}
