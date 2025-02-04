0346f4ab59fed59a0df87b300ec30c36d8ff6fbb
YeaWon Kim <yeawon6566@gmail.com>
2025-02-04 13:01:08 +0900

    [level 2] Title: 숫자의 표현, Time: 1.82 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i=1; i<=n; i++) {
+            int temp = 0;
+            
+            for (int j=i; j<=n; j++) {
+                
+                temp += j;
+                if (temp == n) {
+                    answer++;
+                    break;
+                } else if (temp > n) {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
